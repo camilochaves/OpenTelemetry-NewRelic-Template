9c6e06a0f3997129520325b310099baacc4c2fbf
Camilo Chaves <camilo.chaves@capgemini.com>
2023-07-14 14:51:23 -0300

    Fixed Log Bug

--- startOtelContainer.sh
+++ startOtelContainer.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 clear
+docker network prune --force
+docker network create otel_net
+
 docker kill otel
 docker container prune --force
 echo "Starting OPEN TELEMETRY Container..."
 
-docker run  -d \
+docker run  -d -it \
             -p 4317:4317 \
             --name otel \
             --hostname OtelContainer_HostMetrics \
-            --network sos_net \
+            --network otel_net \
             -v $(pwd)/otel_config.yaml:/app/otel_config.yaml \
             otel/opentelemetry-collector-contrib:latest --config /app/otel_config.yaml
 
